f39c79185eb80033aed8362e865a119184b09d9d
Rahul Roy <70642284+Rahul171201@users.noreply.github.com>
2022-09-24 07:53:35 +0530

    login page created

--- pages/login.js
+++ pages/login.js
@@ -1,16 +1,48 @@
 import styles from '../styles/Login.module.css';
+import { GoogleLogin } from 'react-google-login';
+
 
 export default function login() {
+
+    const responseGoogle = (response) => {
+        console.log(response);
+    }
+
     return (
         <div className={styles.outerContainer}>
+
+            <video autoPlay loop muted plays-inline className={styles.videoBackground}>
+                <source src='video.mp4' type='video/mp4' />
+            </video>
+
             <div className={styles.innerContainer}>
-                <div>Login</div>
-                <form method='post'>
+                <div className={styles.loginText}>Login</div>
+                <form method='post' className={styles.formBox}>
                     <div className={styles.inputBox}>
-                        <label for='email'><b>Email</b></label>
-                        <input type="email" placeholder='Enter your email address' name='email'></input>
+                        <div className={styles.eachInput}>
+                            <label for='email' className={styles.labelText}><b>Email</b></label>
+                            <input type="email" placeholder='Enter your email address' name='email' className={styles.inputField}></input>
+                        </div>
+                        <div className={styles.eachInput}>
+                            <label for='password' className={styles.labelText}><b>Password</b></label>
+                            <input type="password" placeholder='Enter the password' name='password' className={styles.inputField}></input>
+                        </div>
+                        <div className={styles.forgotPasswordBox}>
+                            <span><a href="#">Forgot password</a></span>
+                        </div>
                     </div>
                 </form>
+                <div className={styles.bottomBox}>
+                    <GoogleLogin
+                        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                        buttonText="Login"
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
+                        cookiePolicy={'single_host_origin'}
+                    />
+                    <button className={styles.signupButton}>Sign Up</button>
+
+                </div>
             </div>
         </div>
     )
