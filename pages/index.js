667fac3856628e6972469251fd51d9b17649477b
Rahul Roy <rahulroydps@gmail.com>
2022-10-05 11:18:34 +0530

    homepage started

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,28 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div className={styles.mainContainer}>
+      <div className={styles.navbar}>
+        <div className={styles.logoContainer}>
+          <img src='logo.jpeg' alt='logo' className={styles.logo}></img>
+        </div>
+        <div className={styles.navList} >
+          <button className={styles.loginButton}>LOG IN</button>
+          <button className={styles.signupButton}>SIGN UP</button>
+        </div>
+      </div>
+      <div className={styles.middleContainer}>
+        <div className={styles.contentBox}>
+          <div className={styles.heading}>A single platform that replaces multi-solution chaos and integrates hospital management with patient healthcare!</div>
+          <div className={styles.buttonContainer}>
+            <button className={styles.departmentButton}>View Departments</button>
+            <button className={styles.appointmentButton}>Make an Appointment</button>
+          </div>
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </div>
   )
 }
